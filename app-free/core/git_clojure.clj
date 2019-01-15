9f71ef97d04ddf18ae06915c42cf1f44f22e0ae5
sergeiudris <sergeiudris@gmail.com>
2019-01-15 20:26:00 +0300

    fix app-free, load-codeqs clojure

--- app-free/core/git_clojure.clj
+++ app-free/core/git_clojure.clj
@@ -2,9 +2,10 @@
   (:require [clj-time.core :as t]
             [nrepl.server :refer [start-server stop-server]]
             [clj-time.format :as f]
+            [clojure.pprint :refer [pprint]]
             [datomic.api :as d]))
 
-(def db-uri "datomic:free://datomicdbfree:4334/clojure")
+(def db-uri "datomic:free://datomicdbfree:4334/git")
 
 (d/create-database db-uri)
 (def conn (d/connect db-uri))
@@ -15,31 +16,75 @@
   (println conn)
   (defonce server (start-server :bind "0.0.0.0" :port 7888)))
 
-; (map first
-;      (d/q '[:find ?repo
-;             :where [?e :repo/uri ?repo]]
-;           db))
+(map first
+     (d/q '[:find ?repo
+            :where [?e :repo/uri ?repo]]
+          db))
 
-; (map first
-;      (d/q '[:find ?ns
-;             :where
-;             [?e :clj/ns ?n]
-;             [?n :code/name ?ns]]
-;           db))
+(map first
+     (d/q '[:find ?ns
+            :where
+            [?e :clj/ns ?n]
+            [?n :code/name ?ns]]
+          db))
 
-; (reduce (fn [agg [o d]]
-;           (update-in agg [o] (fnil conj []) d))
-;         {}
-;         (d/q '[:find ?op ?def
-;                :where
-;                [?e :clj/def ?d]
-;                [?e :clj/defop ?op]
-;                [?d :code/name ?def]]
-;              db))
+(reduce (fn [agg [o d]]
+          (update-in agg [o] (fnil conj []) d))
+        {}
+        (d/q '[:find ?op ?def
+               :where
+               [?e :clj/def ?d]
+               [?e :clj/defop ?op]
+               [?d :code/name ?def]]
+             db))
 
-; (defn foo
-;   "I don't do a whole lot."
-;   []
-;   (println "Hello, World!"))
+(defn foo
+  "I don't do a whole lot."
+  []
+  (println "Hello, World!"))
 
-; (foo )
+(foo )
+
+
+
+; (defn -main [& ]
+;   (println "Running clojure-and-contrib examples with database" database-uri)
+;   (try
+;     (let [conn        (d/connect database-uri)
+;           db          (-> conn d/db (d/as-of 435691))
+;           repos       (map first
+;                            (q '[:find ?repo
+;                                 :where [?e :repo/uri ?repo]]
+;                               db))
+;           namespaces  (map first
+;                            (q '[:find ?ns
+;                                 :where
+;                                 [?e :clj/ns ?n]
+;                                 [?n :code/name ?ns]]
+;                               db))
+;           definitions (reduce (fn [agg [o d]]
+;                                 (update-in agg [o] (fnil conj []) d))
+;                               {}
+;                               (q '[:find ?op ?def
+;                                    :where
+;                                    [?e :clj/def ?d]
+;                                    [?e :clj/defop ?op]
+;                                    [?d :code/name ?def]]
+;                                  db))]
+;       (println)
+;       (println "#### Repos:")
+;       (println)
+;       (pprint repos)
+;       (println)
+;       (println "#### Namespaces:")
+;       (println)
+;       (pprint namespaces)
+;       (println)
+;       (println "#### Definitions:")
+;       (println)
+;       (pprint definitions)
+;       (assert (= 44 (-> definitions keys count)))
+;       (assert (= 33 (-> "defne" definitions count))))
+;     (finally
+;       ;; (shutdown-agents)
+;       (System/exit 0))))
--- load-codeqs/load.sh
+++ load-codeqs/load.sh
@@ -3,13 +3,28 @@
 
 echo hello
 
+import_codeq(){
+    java -server -Xmx1g -jar target/codeq-0.1.0-SNAPSHOT-standalone.jar datomic:free://datomicdbfree:4334/codeq
+}
+
+import_clojure(){
+    java -server -Xmx1g -jar codeq-0.1.0-SNAPSHOT-standalone.jar datomic:free://datomicdbfree:4334/git
+}
+
 cd codeq
 lein uberjar
-java -server -Xmx1g -jar target/codeq-0.1.0-SNAPSHOT-standalone.jar datomic:free://datomicdbfree:4334/codeq
+import_codeq &
+# java -server -Xmx1g -jar target/codeq-0.1.0-SNAPSHOT-standalone.jar datomic:free://datomicdbfree:4334/codeq
 cd ../clojure
-echo $pwd
+# echo $pwd
 cp ../codeq/target/codeq-0.1.0-SNAPSHOT-standalone.jar ./
-ls
-java -server -Xmx1g -jar codeq-0.1.0-SNAPSHOT-standalone.jar datomic:free://datomicdbfree:4334/git
+import_clojure &
+
+# ls
+# java -server -Xmx1g -jar codeq-0.1.0-SNAPSHOT-standalone.jar datomic:free://datomicdbfree:4334/git
+
+
+
+
 tail -f /dev/null
 "$@"
