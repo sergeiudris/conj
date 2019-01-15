b569f8e17c104303d56af5ae829c86fe871f0c7e
sergeiudris <sergei.udris@gmail.com>
2019-01-15 15:21:10 +0300

    query rules

--- app-pro/core/mbrainz.clj
+++ app-pro/core/mbrainz.clj
@@ -2,7 +2,8 @@
   (:require [clj-time.core :as t]
             [nrepl.server :refer [start-server stop-server]]
             [clj-time.format :as f]
-            [datomic.api :as d]))
+            [datomic.api :as d]
+            [core.rules :refer (rules)]))
 
 ; (def cfg {:server-type :peer-server
 ;           :access-key "myaccesskey"
@@ -25,6 +26,9 @@
   (defonce server (start-server :bind "0.0.0.0" :port 7888)))
 
 
+;; https://github.com/Datomic/mbrainz-sample/wiki/Queries
+;; https://github.com/Datomic/mbrainz-sample/blob/master/examples/clj/datomic/samples/mbrainz.clj
+
 (d/q '[:find ?id ?type ?gender
        :in $ ?name
        :where
@@ -40,27 +44,27 @@
 ;; What are titles of the tracks John Lennon palyed on?
 
 (def qy1 '[:find ?title
-          :in $ ?artist-name
-          :where
-          [?a :artist/name ?artist-name]
-          [?t :track/artists ?a]
-          [?t :track/name ?title]
-          ])
+           :in $ ?artist-name
+           :where
+           [?a :artist/name ?artist-name]
+           [?t :track/artists ?a]
+           [?t :track/name ?title]])
+          
 
 (d/q qy1 db "John Lennon")
 
 (d/q '[:find ?title ?album ?year
-      :in $ ?artist-name
-      :where
-      [?a :artist/name ?artist-name]
-      [?t :track/artists ?a]
-      [?t :track/name ?title]
-      [?m :medium/tracks ?t]
-      [?r :release/media ?m]
-      [?r :release/name ?album]
-      [?r :release/year ?year]
+       :in $ ?artist-name
+       :where
+       [?a :artist/name ?artist-name]
+       [?t :track/artists ?a]
+       [?t :track/name ?title]
+       [?m :medium/tracks ?t]
+       [?r :release/media ?m]
+       [?r :release/name ?album]
+       [?r :release/year ?year]]
       
-      ]
+      
      db "John Lennon")
 
 
@@ -77,3 +81,37 @@
        [(< ?year 1970)]]
      
      db "John Lennon")
+
+(def rules-custom '[;; Given ?t bound to track entity-ids, binds ?r to the corresponding
+ ;; set of album release entity-ids
+             [(track-release ?t ?r)
+              [?m :medium/tracks ?t]
+              [?r :release/media ?m]]])
+
+; rules
+
+(d/q '[:find ?title ?album ?year
+       :in $ % ?artist-name
+       :where
+       [?a :artist/name ?artist-name]
+       [?t :track/artists ?a]
+       [?t :track/name ?title]
+       (track-release ?t ?r)
+       [?r :release/name ?album]
+       [?r :release/year ?year]]
+     db rules-custom "John Lennon")
+
+(d/q '[:find ?title ?album ?year
+       :in $ % ?artist-name
+       :where
+       [?a :artist/name   ?artist-name]
+       [?t :track/artists ?a]
+       [?t :track/name    ?title]
+       (track-release ?t ?r)
+       [?r :release/name  ?album]
+       [?r :release/year  ?year]]
+     db
+     rules
+     "John Lennon")
+
+
