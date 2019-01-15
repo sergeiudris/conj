10c8b81356b492c9f7931d8f6528dba295062c28
sergeiudris <sergei.udris@gmail.com>
2019-01-15 19:33:40 +0300

    wip app-free

--- app-free/core/git_clojure.clj
+++ app-free/core/git_clojure.clj
@@ -15,31 +15,31 @@
   (println conn)
   (defonce server (start-server :bind "0.0.0.0" :port 7888)))
 
-(map first
-     (d/q '[:find ?repo
-            :where [?e :repo/uri ?repo]]
-          db))
+; (map first
+;      (d/q '[:find ?repo
+;             :where [?e :repo/uri ?repo]]
+;           db))
 
-(map first
-     (d/q '[:find ?ns
-            :where
-            [?e :clj/ns ?n]
-            [?n :code/name ?ns]]
-          db))
+; (map first
+;      (d/q '[:find ?ns
+;             :where
+;             [?e :clj/ns ?n]
+;             [?n :code/name ?ns]]
+;           db))
 
-(reduce (fn [agg [o d]]
-          (update-in agg [o] (fnil conj []) d))
-        {}
-        (d/q '[:find ?op ?def
-               :where
-               [?e :clj/def ?d]
-               [?e :clj/defop ?op]
-               [?d :code/name ?def]]
-             db))
+; (reduce (fn [agg [o d]]
+;           (update-in agg [o] (fnil conj []) d))
+;         {}
+;         (d/q '[:find ?op ?def
+;                :where
+;                [?e :clj/def ?d]
+;                [?e :clj/defop ?op]
+;                [?d :code/name ?def]]
+;              db))
 
-(defn foo
-  "I don't do a whole lot."
-  []
-  (println "Hello, World!"))
+; (defn foo
+;   "I don't do a whole lot."
+;   []
+;   (println "Hello, World!"))
 
-(foo )
+; (foo )
