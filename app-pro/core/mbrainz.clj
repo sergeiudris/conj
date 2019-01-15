10ce906ec907b1902b6ddbda4869b71bc4160978
sergeiudris <sergei.udris@gmail.com>
2019-01-15 14:49:49 +0300

    querying

--- app-pro/core/mbrainz.clj
+++ app-pro/core/mbrainz.clj
@@ -49,3 +49,18 @@
 
 (d/q qy1 db "John Lennon")
 
+(d/q '[:find ?title ?album ?year
+      :in $ ?artist-name
+      :where
+      [?a :artist/name ?artist-name]
+      [?t :track/artists ?a]
+      [?t :track/name ?title]
+      [?m :medium/tracks ?t]
+      [?r :release/media ?m]
+      [?r :release/name ?album]
+      [?r :release/year ?year]
+      
+      ]
+     db "John Lennon")
+
+
