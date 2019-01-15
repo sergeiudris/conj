78ab6753d7e232749cdb2bc26223b1dedf352a76
sergeiudris <sergei.udris@gmail.com>
2019-01-15 14:20:46 +0300

    first mbrainz query

--- app-pro/core/mbrainz.clj
+++ app-pro/core/mbrainz.clj
@@ -10,16 +10,30 @@
 ;           :endpoint "datomicdb:4334"})
 
 ; (def client (d/client cfg))
-(def db-uri "datomic:dev://datomicdb:4334/hello")
+(def db-uri "datomic:dev://datomicdb:4334/mbrainz-1968-1973")
 
 
 (d/create-database db-uri)
 (def conn (d/connect db-uri))
 ; (def conn (d/connect client {:db-name "hello"}))
 
+(def db (d/db conn))
 
 
+(d/q '[:find ?id ?type ?gender
+       :in $ ?name
+       :where
+       [?e :artist/name ?name]
+       [?e :artist/gid ?id]
+       [?e :artist/type ?teid]
+       [?teid :db/ident ?type]
+       [?e :artist/gender ?geid]
+       [?geid :db/ident ?gender]]
+     db
+     "Janis Joplin")
 
 (defn -main [] 
   (println conn)
   (defonce server (start-server :bind "0.0.0.0" :port 7888)))
+
+
