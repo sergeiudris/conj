f4f2e65d3cdc007e9525628d67bc9651da3d791f
sergeiudris <sergeiudris@gmail.com>
2019-01-16 05:34:53 +0300

    learn clojure: syntax namespcae

---
+++ app-clj/core/learn_syntax.clj
@@ -0,0 +1,14 @@
+
+(ns learn-syntax
+   (:require [nrepl.server :refer [start-server stop-server]]
+             [clojure.pprint :refer [pprint]]))
+
+
+42
+
+(println 42)
+
+
+(defn -main []
+  (println "running learn-syntax")
+  (defonce server (start-server :bind "0.0.0.0" :port 7888)))
