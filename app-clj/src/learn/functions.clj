aed725f8dbf5407f1a99fac1cedc024dec817e5c
sergeiudris <sergeiudris@gmail.com>
2019-01-16 05:58:44 +0300

    learn.functions ns

---
+++ app-clj/src/learn/functions.clj
@@ -0,0 +1,29 @@
+
+(ns learn.functions
+   (:require [nrepl.server :refer [start-server stop-server]]
+             [clojure.pprint :refer [pprint]]
+             [clojure.repl :refer :all]))
+
+
+
+(def greet (fn [name] (str "Hello " name)))
+
+(greet "q")
+
+;; equivalent ot (fn [x] (+ 6 x))
+#(+ 6 %)
+
+(fn [x y] (+ x y))
+
+#(+ %1 %2)
+
+(fn [x y & zs] (println x y zs))
+
+#(println %1 %2 %&)
+
+(#(vector %) 1)
+
+
+(defn -main []
+  (println "running learn.functions")
+  (defonce server (start-server :bind "0.0.0.0" :port 7888)))
--- app-clj/src/learn/syntax.clj
+++ app-clj/src/learn/syntax.clj
@@ -1,12 +1,23 @@
 
 (ns learn.syntax
    (:require [nrepl.server :refer [start-server stop-server]]
-             [clojure.pprint :refer [pprint]]))
+             [clojure.pprint :refer [pprint]]
+             [clojure.repl :refer :all]))
 
 
-42
+42 ; Long - 64 bit integer from -2^63 to 2^63-1
+
+6.022e23 ; Double  - double-precision 64-bit floating point 
+42N ; BigInt - arbirtrary precision integer
+1.0M ; BigDecimal arbitrary precision fixed point
+22/7 ; Ration
+"hello" ; String
+\e ; character
+
 
 (println 42)
+
+(doc +)
 
 
 (defn -main []
