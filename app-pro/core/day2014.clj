b4e2b82a1990203f20c60dfa9f3c07ff939985cf
sergeiudris <sergeiudris@gmail.com>
2019-01-19 08:10:22 +0300

    quick recap of movies using client api

--- app-pro/core/day2014.clj
+++ app-pro/core/day2014.clj
@@ -28,6 +28,44 @@
   
   db
   
+  (def movie-schema [{:db/ident :movie/title
+                      :db/valueType :db.type/string
+                      :db/cardinality :db.cardinality/one
+                      :db/doc "The title of the movie"}
+
+                     {:db/ident :movie/genre
+                      :db/valueType :db.type/string
+                      :db/cardinality :db.cardinality/one
+                      :db/doc "The genre of the movie"}
+
+                     {:db/ident :movie/release-year
+                      :db/valueType :db.type/long
+                      :db/cardinality :db.cardinality/one
+                      :db/doc "The year the movie was released in theaters"}])
+  
+  (d/transact conn {:tx-data movie-schema})
+  
+  (def first-movies [{:movie/title "The Goonies"
+                      :movie/genre "action/adventure"
+                      :movie/release-year 1985}
+                     {:movie/title "Commando"
+                      :movie/genre "action/adventure"
+                      :movie/release-year 1985}
+                     {:movie/title "Repo Man"
+                      :movie/genre "punk dystopia"
+                      :movie/release-year 1984}])
+  
+  (d/transact conn {:tx-data first-movies})
+  
+  (def db (d/db conn))
+  
+  db
+  
+  (def all-movies-q '[:find ?e
+                      :where [?e :movie/title]])
+  
+  (d/q all-movies-q db)
+  
   
   
   )
