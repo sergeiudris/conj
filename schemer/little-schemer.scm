ed5897122f33a527e1b1128188d144fdb1c505d5
sergeiudris <sergei.udris@gmail.com>
2019-02-20 15:38:14 +0300

    partial and total functions

--- schemer/little-schemer.scm
+++ schemer/little-schemer.scm
@@ -1634,3 +1634,73 @@ The First Commandment:
        )
     )
 )
+
+(define align 
+    (lambda (pora)
+        (cond 
+            ((atom? pora) pora)
+            ((a-pair? (first pora))  (align (shift pora))  )
+            (else (build (first pora) (align (second pora))))
+        )
+    )
+)
+
+(define length*
+    (lambda (pora)
+    (cond
+    ( ( atom? pora) 1 )
+    (else
+    ( + (length* (first pora) )
+    ( length* (second pora ) ) ) ) ) ) )
+
+(define weight*
+    (lambda (pora)
+    (cond
+    ( ( atom? pora) 1)
+    (else
+    ( + ( * ( weight* (first pora) ) 2)
+    ( weight* (second pora ) ) ) ) ) ) )
+
+
+(define shuffle
+    (lambda (pora)
+    (cond
+    ( ( atom? pora) pora)
+    ( ( a-pair? (first pora))
+    ( shujjle ( revpair pora)))
+    (else ( build (first pora)
+    ( shuffle ( second pora) ) ) ) ) ) )
+
+(define C
+    (lambda (n)
+    (cond
+    ( ( one? n) 1 )
+    (else
+    (cond
+    (( even? n) ( C (+ n 2 ) ) )
+    (else ( C ( add1 ( * 3 n)))) ) ) ) ) )
+
+(define A
+    (lambda (n m)
+    (cond
+    ( ( zero? n) ( add1 m))
+    ( ( zero? m) ( A ( sub1 n) 1))
+    (else ( A (sub1 n)
+    ( A n ( sub1 m ) ) ) ) ) ) )
+
+;; A is a total function because it always gives an answer
+
+(define will-stop? 
+    (lambda (f)
+
+    
+    )
+    
+    )
+
+(define last-try 
+    (lambda (x)
+        (and (will-stop? last-try) (eternity x))
+    )
+    
+)
