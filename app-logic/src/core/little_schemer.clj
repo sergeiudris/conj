1c14b22f0d8b4d8680b9601ff4bff5486ecda65d
sergeiudris <sergei.udris@gmail.com>
2019-02-21 14:23:20 +0300

    little-schemer.clj member?

--- app-logic/src/core/little_schemer.clj
+++ app-logic/src/core/little_schemer.clj
@@ -1,6 +1,85 @@
-(ns core.little-schemer 
-  (:require [clojure.repl :refer :all]))
+(ns core.little-schemer )
 
-(defn atom? [a]
+;; 1. TOYS
+
+(defn atom? 
+  [a]
   (not (coll? a)))
+
+(atom? '())
+
+;; https://en.wikipedia.org/wiki/CAR_and_CDR
+
+;; Contents of the Address Register
+(def car first)
+
+;; Contents of the Decrement Register
+(def cdr rest)
+
+
+(cons 1 '( 2 3))
+
+;; list composed of zero S-expressions
+(def null? empty?)
+; (defn null? [l]
+;   (cond
+;     ( and (coll? l) (empty? l) ) true
+;     :else false
+;     )
+;   )
+
+(null? '(1 2))
+
+(defn eq? 
+  [a b] 
+  (cond
+    (or (number? a) (number? b) ) (throw (Exception. "eq? takes two arguments. Both of them must be non-numeric atoms"))
+    (and (atom? a) (atom? b) (= a b)) true
+    :else false
+    )
+  )
+
+(eq? 'a (car '(a b c)))
+
+(eq? 'ab 'ab)
+
+(eq? 1 1)
+
+(number? 1)
+
+(eq? '() '())
+
+;; 2. Do It, Do It Again
+
+(null? 'a)
+
+;; is list a list of atoms
+(defn lat? 
+  [l]
+  (cond 
+    (null? l) true
+    (atom? (car l)) (lat? (cdr l))
+    :else false
+    )
+  )
+
+(lat? '(a b c))
+(lat? '(a 1))
+(lat? '(a (1)))
+
+
+(defn member?
+  [a lat]
+  (cond
+    (null? lat) false
+    (eq? (car lat) a) true
+    :else (member? a (cdr lat))
+    )
+  )
+
+(member? 'z '(a b c z))
+
+
+
+
 
