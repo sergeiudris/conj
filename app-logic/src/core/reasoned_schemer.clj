edc2a89600338ae4a7a8db4ab1408ffaed66e059
sergeiudris <sergeiudris@gmail.com>
2019-02-26 08:59:05 +0300

    reasoned chemer using core logic

--- app-logic/src/core/reasoned_schemer.clj
+++ app-logic/src/core/reasoned_schemer.clj
@@ -1,6 +1,66 @@
 (ns core.reasoned-schemer
-  (:use core.little-schemer)
+  (:require [ core.little-schemer :as lscm]
+             [clojure.core.logic :refer :all]
+             [clojure.repl :refer :all]
+              )
   )
 
 (defn hey [] "hey")
+
+(hey)
+
+(doc succeed)
+
+(succeed 3)
+
+(run* [q]
+      succeed)
+
+(run* [q]
+      fail)
+
+(source -run)
+
+
+(run* [q]
+      (== true q)
+      )
+
+(run* [q]
+      succeed
+      (== true q))
+
+
+(run* [q]
+      fail
+      (== true q))
+
+(run* [r]
+      succeed
+      (== :corn r)
+      )
+
+(run* [q]
+       (let
+        [x true]
+         (== false x)))
+
+
+(run* [q]
+ (fresh [x]
+  (== true x)
+  (== true q)
+  )
+ )
+
+(run* [x]
+      (let 
+       [x false]
+        (fresh [x]
+               (== true x)
+               )
+        ))
+
+
+
 
