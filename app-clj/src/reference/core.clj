f6d50c3bf1896cf4e2f25245c0066e29fe267d71
sergeiudris <sergeiudris@gmail.com>
2019-03-12 07:57:04 +0300

    protocols

--- app-clj/src/reference/core.clj
+++ app-clj/src/reference/core.clj
@@ -300,3 +300,47 @@
   (area {})
   
   )
+
+
+(comment
+  
+  ;; https://clojure.org/reference/protocols
+  
+  (defprotocol P
+    (foo [x] [x y])
+    (bar-me [x] [x y])
+    )
+  
+  (deftype Foo [a b c]
+    P
+    (foo [x] a)
+    (bar-me [x] b)
+    (bar-me [x y] (+ c y))
+    )
+  
+  (bar-me (Foo. 1 2 3) 42)
+  
+  (foo 
+   (let [x 42]
+     (reify P
+       (foo [this] 17)
+       (bar-me [this] x)
+       (bar-me [this y] x)
+       )
+     )
+   )
+  
+  (foo
+   (let [x 42]
+     (reify P
+       (foo [this] 17)
+       (foo [this y] (+ x y))
+       (bar-me [this] x)
+       (bar-me [this y] x)))
+   11
+   )
+  
+  
+  
+  
+  )
